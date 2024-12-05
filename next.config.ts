3064ea8995c6e22d7c273d9d144325fc4a5802b9
Quentin <154846367+notro636@users.noreply.github.com>
2024-12-05 12:58:29 -0600

    Update and rename next.config.js to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,7 @@
-const nextConfig = {
+import type { NextConfig } from 'next'
+ 
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
     // your project has ESLint errors.
