88da65113cdb8b701c61d38ce437009802102e5e
Quentin <154846367+notro636@users.noreply.github.com>
2024-12-05 13:01:12 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,4 @@
-import type { NextConfig } from 'next'
- 
-const nextConfig: NextConfig = {
+const nextConfig = {
   reactStrictMode: true,
   eslint: {
     // Warning: This allows production builds to successfully complete even if
